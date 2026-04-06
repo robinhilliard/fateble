e6e6697eb6eed536b8880ad2d9d2ccfa4f937559
robinhilliard <rfb@rcb0apd@tuz7ZPX>
2026-04-06 19:49:45 +1000

    Add browser tests environment variable and update test helper logic
    - Introduced a new environment variable `BROWSER_TESTS` in the GitHub Actions workflow to enable browser testing.
    - Modified the test helper to conditionally start the Wallaby application based on the presence of the `BROWSER_TESTS` variable, enhancing test flexibility.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-unless System.get_env("GITHUB_ACTIONS") do
+if !System.get_env("GITHUB_ACTIONS") || System.get_env("BROWSER_TESTS") do
   {:ok, _} = Application.ensure_all_started(:wallaby)
   Application.put_env(:wallaby, :base_url, FateWeb.Endpoint.url())
 end
